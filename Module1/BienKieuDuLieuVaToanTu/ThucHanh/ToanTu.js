21642d0a0c92c80872ad5a7696f949e37460ed1c
hieunguyen164 <hieunguyen16491@gmail.com>
2020-02-27 08:38:56 +0700

    bai tap module1

--- Module1/BienKieuDuLieuVaToanTu/ThucHanh/ToanTu.js
+++ Module1/BienKieuDuLieuVaToanTu/ThucHanh/ToanTu.js
@@ -1,8 +1,4 @@
-let inputWidth;
-let inputHeight;
 inputWidth = prompt("Enter the Width: ");
 inputHeight = prompt("Enter the height: ");
-let width = parseInt(inputWidth);
-let height = parseInt(inputHeight);
-let area = width * height;
+let area = parseInt(inputWidth) * parseInt(inputHeight);
 document.write("The area is: " + area);
