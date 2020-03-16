be4ab7f392f36138593bcc212b1ec774f718d1b7
hieunguyen164 <hieunguyen16491@gmail.com>
2020-03-16 09:37:44 +0700

    Bai tap Module 1

--- Module1/Ham/BaiTap/QuanLySanPham.js
+++ Module1/Ham/BaiTap/QuanLySanPham.js
@@ -3,42 +3,37 @@ let product = ['iPhone 7', 'iPhone 7 Plus', 'iPhone 8', 'iPhone 8 Plus'];
 function addItem() {
     let newItem = document.getElementById('newItem').value;
     product.push(newItem);
-    alert(newItem + ' da duoc them vao danh sach.');
+    alert(newItem + ' added successfull.');
     document.getElementById('newItem').value='';
 }
-
-function deleteItem() {
-    let del = prompt('Nhap ten san pham muon xoa:');
+function deleteItem(x) {
     for (let i = 0; i < product.length; i++) {
-        if (del == product[i]) {
+        if (x == i) {
             product.splice(i, 1);
+            alert('Deleted');
         }
     }
-    alert('Xoa ' + del+' thanh cong')
     displayItem();
-
 }
+function editItem(x) {
 
-function editItem() {
-    let editOld = prompt('Nhap ten san pham muon chinh sua: ');
     let editNew = prompt('Nhap ten san pham moi:');
     for (let i = 0; i < product.length; i++) {
-        if (editOld == product[i]) {
+        if (x==i) {
             product.splice(i, 1, editNew);
+            alert(' Rename: '+editNew);
         }
     }
-    alert(editOld +' da duoc doi ten thanh '+editNew);
     displayItem();
 }
-
 function displayItem() {
     let display = '<table class="product">';
     for (let i = 0; i < product.length; i++) {
         display += '<tr>';
         for (let j = 0; j < 1; j++) {
             display += '<td>' + product[i] + '</td>'
-                + '<td>' + '<button class="btn" onclick="editItem()">' + 'EDIT' + '</button>' + '</td>'
-                + '<td>' + '<button class="btn" onclick="deleteItem()">' + 'DELETE' + '</button>' + '</td>';
+                + '<td>' + '<button class="btn" onclick="editItem(' + i + ')">' + 'EDIT' + '</button>' + '</td>'
+                + '<td>' + '<button class="btn" onclick="deleteItem(' + i + ')">' + 'DELETE' + '</button>' + '</td>';
         }
         display+='</tr>';
     }
